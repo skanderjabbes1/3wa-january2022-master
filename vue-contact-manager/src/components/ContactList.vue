88ff5a3ecb3f6a3fef207bd9c2ebff2bd7756359
Houcem Hedhly <hedhoucem@gmail.com>
2022-02-10 15:34:08 +0100

    display contact list

---
+++ vue-contact-manager/src/components/ContactList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1><fa-icon icon="address-book"></fa-icon> Contact List</h1>
+    <ul class="list-group">
+      <li
+        v-for="contact in list"
+        v-bind:key="contact.id"
+        class="list-group-item"
+      > {{ contact.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'ContactList',
+    props: {
+        list: Array
+        },
+}
+</script>
+
+<style>
+
+</style>

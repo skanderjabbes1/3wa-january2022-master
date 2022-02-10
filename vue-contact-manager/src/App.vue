480deb2f47116129d4450c56bb255b7032e53ed1
Houcem Hedhly <hedhoucem@gmail.com>
2022-02-10 16:36:25 +0100

    export navbar in a seperate component

--- vue-contact-manager/src/App.vue
+++ vue-contact-manager/src/App.vue
@@ -1,31 +1,13 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> | 
-    <router-link to="/contact">Contact</router-link>
-  </div>
-  <router-view/>
+  <NavBar></NavBar>
+  <router-view class="container py-3"/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import NavBar from '@/components/NavBar.vue';
+export default {
+  components: {
+    NavBar,
+  },
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>

422c27ea7e0d72731216030223b404ea5a3f93a3
Houcem Hedhly <hedhoucem@gmail.com>
2022-02-11 11:11:34 +0100

    create ContactDetailss component

---
+++ vue-contact-manager/src/components/ContactDetails.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="card" style="width: 28rem;">
+  <div class="card-body">
+    <h5 class="card-title"> {{ contact.name }}</h5>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item"> {{ contact.email }}</li>
+    <li class="list-group-item"> {{ contact.phone }}</li>
+    <li class="list-group-item"> {{ contact.address.street }}</li>
+    <li class="list-group-item"> {{ contact.address.city }}</li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+    props: ['contact']
+}
+</script>
+
+<style>
+
+</style>

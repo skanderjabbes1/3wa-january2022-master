5f5864f7feb18561eae2e069b11cbe63d88a9d9b
Houcem Hedhly <hedhoucem@gmail.com>
2022-02-11 14:31:03 +0100

    create form for AddContact component

--- vue-contact-manager/src/components/AddContact.vue
+++ vue-contact-manager/src/components/AddContact.vue
@@ -1,5 +1,31 @@
 <template>
-  <h2>Add contact component</h2>
+  <form>
+        <div class="mb-3">
+            <label for="contactName" class="form-label">Name</label>
+            <input type="text" class="form-control" id="contactName">
+        </div>
+        <div class="row mb-3">
+            <div class="col">
+                <label for="contactEmail" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="contactEmail">
+            </div>
+            <div class="col">
+                <label for="contactPhone" class="form-label">Phone number</label>
+                <input type="text" class="form-control" id="contactPhone">
+            </div>
+        </div>
+        <div class="row mb-3">
+            <div class="col">
+                <label for="contactStreetAddress" class="form-label">Street Address</label>
+                <input type="text" class="form-control" id="contactStreetAddress">
+            </div>
+            <div class="col">
+                <label for="contactCity" class="form-label">City</label>
+                <input type="text" class="form-control" id="contactCity">
+            </div>
+        </div>
+        <button type="submit" class="btn btn-primary">Add</button>
+    </form>
 </template>
 
 <script>
